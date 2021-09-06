a83190b0afbb16cece57fc0c0b778f983e6397be
AfRiy <afriy@example.com>
2021-09-06 09:06:10 +0200

    Init

---
+++ src/App.java
@@ -0,0 +1,14 @@
+/*
+* File: ButtonPanel.java
+* Author: Madarász Dávid
+* Copyright: 2021, Madarász Dávid
+* Group: Szoft V
+* Date: 2021-09-03
+* Github: https://github.com/afriyy/
+* Licenc: GNU GPL
+*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
